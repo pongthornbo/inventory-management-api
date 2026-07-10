04cf34d45803a3b97d25a03a514647222c4978bd
Pongthorn Boonrod <pongthorn.bo@gmail.com>
2026-07-10 16:43:30 +0700

    feat(db): add alembic migration setup

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 
-from app.database import Base, engine
 from app.models import product
 from app.routers import products
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
